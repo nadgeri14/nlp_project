757cc92436584924552864bb3d96cb3f4f752672
Abhishek Nadgeri <abhishek22596@gmail.com>
2023-07-05 20:23:26 +0200

    changes to add GPS

--- dataset_code/src/training_graph.py
+++ dataset_code/src/training_graph.py
@@ -124,18 +124,19 @@ def train(epoch):
             time_steps = val_sample.window
             adj = None
 
-        if(args.gnn == 'gat'):
-            output = model(all_graphs, val_features, time_steps, adj)
-        elif(args.gnn == 'transformer'):
-            pe = []
-            transform = T.AddRandomWalkPE(walk_length=20, attr_name='pe')
-            for k in range(time_steps):
-                data = Data(x=val_features[k], edge_index=all_graphs[k])
-                pe.append(transform(data))
-            batch = torch.zeros(val_features[0].shape[0], dtype=torch.int64, device=val_features.device)
-            output = model(val_features, pe, all_graphs, time_steps, batch)
-        else:
-            raise Exception("Select right model")
+            if(args.gnn == 'gat'):
+                output = model(all_graphs, val_features, time_steps, adj)
+            elif(args.gnn == 'transformer'):
+
+                pe = []
+                transform = T.AddRandomWalkPE(walk_length=20, attr_name='pe')
+                for k in range(time_steps):
+                    data = Data(x=val_features[k], edge_index=all_graphs[k])
+                    pe.append(transform(data))
+                batch = torch.zeros(val_features[0].shape[0], dtype=torch.int64, device=val_features.device)
+                output = model(val_features, pe, all_graphs, time_steps, batch)
+            else:
+                raise Exception("Select right model")
             #loss_val = F.nll_loss(output, val_label)
             loss_val = loss_fn(output, val_label,  get_samples_per_class(val_label))
             accuracy_val.append(accuracy(output, val_label).detach().cpu().numpy())
